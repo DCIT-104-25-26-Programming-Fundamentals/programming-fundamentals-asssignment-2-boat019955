0778288c10569ebc5f70a9501ff495ee2709413e
boat019955 <boat019955Z@gmail.com>
2026-07-31 21:20:49 +0000

    Add functions to print multiplication tables

--- assignment_06_multiplication_table.cpp
+++ assignment_06_multiplication_table.cpp
@@ -56,4 +56,63 @@
 
 #include <iostream>
 using namespace std;
+#include <iostream>
+using namespace std;
 
+// Part A: Print a single multiplication table
+void printSingleTable(int number)
+{
+    cout << "\nMultiplication Table for " << number << ":\n";
+
+    for (int i = 1; i <= 12; i++)
+    {
+        cout << number << " x " << i << " = " << number * i << endl;
+    }
+}
+
+// Part B: Print multiplication tables from 1 to N
+void printTablesUpToN(int n)
+{
+    for (int num = 1; num <= n; num++)
+    {
+        cout << "\nMultiplication Table for " << num << ":\n";
+
+        for (int i = 1; i <= 12; i++)
+        {
+            cout << num << " x " << i << " = " << num * i << endl;
+        }
+
+        cout << "---------------------------" << endl;
+    }
+}
+
+int main()
+{
+    int number, n;
+
+    // Part A
+    cout << "Enter a number for a multiplication table: ";
+    cin >> number;
+
+    if (number <= 0)
+    {
+        cout << "Error: Please enter a positive integer." << endl;
+        return 1;
+    }
+
+    printSingleTable(number);
+
+    // Part B
+    cout << "\nEnter N for tables from 1 to N: ";
+    cin >> n;
+
+    if (n <= 0)
+    {
+        cout << "Error: Please enter a positive integer." << endl;
+        return 1;
+    }
+
+    printTablesUpToN(n);
+
+    return 0;
+}
